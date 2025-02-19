5ca3f23c8677476221bb0df57e397034f8ca47b3
timothydmorel <timothydmorel@gmail.com>
2025-02-18 19:47:49 -0500

    problem with import, board prints

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,2 +1,11 @@
-### CHESS APP ###
-print("hello chess")
+"""Module instantiating a chess board with standard game logic."""
+
+import chess
+
+def main() -> None:
+    """main module to organize tool and initialize instances of board and game logic,
+      make connections to databases, frontend, call utils and services, etc."""
+    board = chess.Board()
+
+    print(board.legal_moves)
+main()
